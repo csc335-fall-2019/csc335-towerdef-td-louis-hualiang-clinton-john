da7ffb69b9b071f24bb06ecee6a4594fe92b0228
louisgalluzzi <42557043+louisgalluzzi@users.noreply.github.com>
2019-11-25 15:00:08 -0700

    Started gui, makes map and side menu.

--- src/TDView.java
+++ src/TDView.java
@@ -1,7 +1,34 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -23,14 +50,59 @@ import javafx.stage.Stage;
  * @author John Stockey
  */
 public class TDView extends Application implements Observer {
+	
+	
+	private  TDController controller;
+	private GridPane mainGrid;
+	private List<List<Rectangle>> gridBoard; // Index is row column style
+	private GridPane menu;
+	public static int COLMAX = 9;
+	public static int ROWMAX = 5;
 	/**
 	 * Purpose: Main window view.
 	 * 
 	 * @param primaryStage Stage of the main window to add scenes to.
+	 *
 	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO
+		TDModel model = new TDModel();
+		this.controller = new TDController(model);
+		
+		
+		MenuBar toolbar = new MenuBar();
+		Menu fileMenu = new Menu("File");
+		MenuItem newGame = new MenuItem("New Game");
+		
+		buildMainGridPane();
+		buildMenu();
+		
+		// Add items to each other
+		fileMenu.getItems().add(newGame);
+		toolbar.getMenus().add(fileMenu);
+		
+		// Add event to newGame
+		//newGameEvent(newGame);
+		
+		// VBox to hold the toolbar and mainGrid
+		//HBox root = new HBox(3);
+		BorderPane root = new BorderPane();
+		root.setTop(toolbar);
+		root.setCenter(mainGrid);
+		root.setLeft(menu);
+		
+		// Create the scene
+		Scene scene = new Scene(root);
+		
+		
+		// Setup and show the window
+		primaryStage.setTitle("Knights vs Zombies");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		
+		
 	}
 	
 	/**
@@ -44,6 +116,88 @@ public class TDView extends Application implements Observer {
 		// TODO
 	}
 	
+	private void buildMainGridPane() {
+		int alternate = 0;
+		mainGrid = new GridPane();
+		this.gridBoard = new ArrayList<List<Rectangle>>();
+		
+		// Set the properties of the grid
+		mainGrid.setHgap(0);
+		mainGrid.setVgap(0);
+		
+		
+		mainGrid.setPadding(new Insets(0));
+		
+		// Fill the grid with Circles
+		for (int rowIndex = 0; rowIndex < ROWMAX; rowIndex++) {
+			// New gridBoard row
+			gridBoard.add(new ArrayList<Rectangle>());
+			
+			for (int colIndex = 0; colIndex < COLMAX; colIndex++) {
+				// New circle representing a token slot
+				Rectangle slot = new Rectangle();
+				Image image;
+				if(alternate == 0) {
+					image = new Image("grass.jpg");
+					alternate = 1;
+				}else {
+					image = new Image("light grass.jpg");
+					alternate = 0;
+				}
+				
+				slot.setFill(new ImagePattern(image));
+				slot.setStroke(Color.BLACK);
+				slot.setHeight(80);
+				slot.setWidth(80);
+				
+				// Add the slot to the grid and to the gridBoard
+				mainGrid.add(slot, colIndex, rowIndex);
+				gridBoard.get(rowIndex).add(slot);
+			}
+		}
+		
+		// Disable the grid for initial launches
+		mainGrid.setDisable(true);
+	}
+	
+	
+	private void buildMenu() {
+		
+		menu = new GridPane();
+		
+		
+		// Set the properties of the grid
+		menu.setHgap(0);
+		menu.setVgap(0);
+		
+		
+		menu.setPadding(new Insets(0));
+		
+		// Fill the grid with Circles
+		for (int rowIndex = 0; rowIndex < 5; rowIndex++) {
+			// New gridBoard row
+			gridBoard.add(new ArrayList<Rectangle>());
+			
+			for (int colIndex = 0; colIndex < 2; colIndex++) {
+				// New circle representing a token slot
+				Rectangle slot = new Rectangle();
+				
+				slot.setFill(Color.GREENYELLOW);
+				slot.setStroke(Color.BLACK);
+				slot.setHeight(80);
+				slot.setWidth(80);
+				
+				// Add the slot to the grid and to the gridBoard
+				menu.add(slot, colIndex, rowIndex);
+				gridBoard.get(rowIndex).add(slot);
+			}
+		}
+		
+		// Disable the grid for initial launches
+		menu.setDisable(true);
+		
+		
+	}
 	
 	/************************** Private Fields Block ***************************/
 	
