66e6b4670fc920cc2fad58711c017043b2f6d947
John Stockey <harujiro@email.arizona.edu>
2019-11-25 11:54:58 -0700

    Java project setup

---
+++ src/TDView.java
@@ -0,0 +1,46 @@
+import java.util.Observable;
+import java.util.Observer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Purpose: GUI window visual for tower defense.
+ * 
+ * <pre>
+ * Uses JavaFX to run a visual game of tower defense.
+ * 
+ * Public Methods:
+ *   start(Stage primaryStage) - Main window view.
+ *   update(Observable model, Object target) - Part of the MVC setup to update according to model.
+ *   Getters and Setters
+ * </pre>
+ * 
+ * 
+ * @author Hualiang Qin
+ * @author Louis Galluzzi
+ * @author Clinton Kral
+ * @author John Stockey
+ */
+public class TDView extends Application implements Observer {
+	/**
+	 * Purpose: Main window view.
+	 * 
+	 * @param primaryStage Stage of the main window to add scenes to.
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO
+	}
+	
+	/**
+	 * Purpose: Part of the MVC setup to update according to model.
+	 * 
+	 * @param model Observable being observed.
+	 * @param target Object depicting the change to update in response to.
+	 */
+	@Override
+	public void update(Observable model, Object target) {
+		// TODO
+	}
+}
