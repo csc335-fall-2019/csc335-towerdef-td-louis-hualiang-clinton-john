aefa816140acfa4147002f9ee68f96ca9bbb60a7
hualiangqin <hualiangqin@email.arizona.edu>
2019-12-03 14:56:52 -0700

    queue up the enemy, modify the entity class

--- src/model/entity/Entity.java
+++ src/model/entity/Entity.java
@@ -19,12 +19,17 @@ import javafx.scene.image.Image;
  * @author Clinton Kral
  * @author John Stockey
  */
-public class Entity extends Observable{
+public class Entity {
 	private String type;
 	private String base;
 	private boolean isValid;
 	private Image image;
-	// More variables for entity specific elements
+	// More variables for entity specific elementsprivate int health;
+	private int health;
+	private int attack;
+	private int speed;
+	private int price;
+	
 	
 	/**
 	 * Purpose: New entity type.
@@ -53,26 +58,44 @@ public class Entity extends Observable{
 				// Basic starting tower
 				this.base = "tower";
 				this.image = new Image("images/tower0.png");
+				this.health = 100;
+				this.attack = 10;
+				this.price = 110;
 			}
 			else if (this.type.equals("tower1")) {
 				this.base = "tower";
 				this.image = new Image("images/tower1.png");
+				this.health = 90;
+				this.attack = 30;
+				this.price = 120;
 			}
 			else if (this.type.equals("tower2")) {
 				this.base = "tower";
 				this.image = new Image("images/tower2.png");
+				this.health = 160;
+				this.attack = 50;
+				this.price = 210;
 			}
 			else if (this.type.equals("tower3")) {
 				this.base = "tower";
 				this.image = new Image("images/tower3.png");
+				this.health = 180;
+				this.attack = 65;
+				this.price = 245;
 			}
 			else if (this.type.equals("tower4")) {
 				this.base = "tower";
 				this.image = new Image("images/tower4.png");
+				this.health = 200;
+				this.attack = 135;
+				this.price = 335;
 			}
 			else if (this.type.equals("tower5")) {
 				this.base = "tower";
 				this.image = new Image("images/tower5.png");
+				this.health = 352;
+				this.attack = 0;
+				this.price = 90;
 			}
 		}
 		
@@ -81,6 +104,27 @@ public class Entity extends Observable{
 			if (this.type.equals("enemy0")) {
 				this.base = "enemy";
 				this.image = new Image("images/enemy0.png");
+				this.health = 300;
+				this.attack = 5;
+				this.speed = 50;
+			}else if (this.type.equals("enemy1")) {
+				this.base = "enemy";
+				this.image = new Image("images/enemy0.png");
+				this.health = 200;
+				this.attack = 50;
+				this.speed = 80;
+			}else if (this.type.equals("enemy2")) {
+				this.base = "enemy";
+				this.image = new Image("images/enemy0.png");
+				this.health = 500;
+				this.attack = 50;
+				this.speed = 20;
+			}else if (this.type.equals("enemy3")) {
+				this.base = "enemy";
+				this.image = new Image("images/enemy0.png");
+				this.health = 100;
+				this.attack = 10;
+				this.speed = 110;
 			}
 		}
 		
@@ -102,6 +146,28 @@ public class Entity extends Observable{
 			// Unsuccessful creation
 			return false;
 		}
+	}
+	
+	/**
+	 * Purpose: decrease the health of the entity when it is attacked
+	 * 
+	 * @param damage
+	 */
+	public void beAttacked(int damage) {
+		if (health > damage) {
+			health -= damage;
+		}else {
+			health = 0;
+		}
+	}
+	
+	/**
+	 * return true when the health is zero
+	 * 
+	 * @return
+	 */
+	public boolean isDead() {
+		return health == 0;
 	}
 	
 	/************************ Getters and Setters Block ************************/
@@ -140,5 +206,43 @@ public class Entity extends Observable{
 	 */
 	public Image getImage() {
 		return this.image;
+	}
+	
+	/**
+	 * getter of the health
+	 * @return
+	 */
+	public int getHealth() {
+		return health;
+	}
+	
+	/**
+	 * getter of the attack
+	 * @return
+	 */
+	public int getAttack() {
+		return attack;
+	}
+	
+	/**
+	 * getter of the price, only tower have the price
+	 * @return
+	 */
+	public int getPrice() {
+		if (this.base.equals("tower")) {
+			return price;
+		}
+		return 0;
+	}
+	
+	/**
+	 * getter of the speed, only enemy have the speed
+	 * @return
+	 */
+	public int getSpeed() {
+		if (this.base.equals("enemy")) {
+			return speed;
+		}
+		return 0;
 	}
 }
